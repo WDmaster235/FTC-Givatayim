6bcd3f53cd605c3d5dda229c16744ed36edbd9cf
WDmaster235 <shalevikichli@gmail.com>
2024-09-25 20:57:54 +0300

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOne.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+//Its just like the "Pose2d" class but ours, so that we can make changes and additions in the class easily.
+import org.firstinspires.ftc.teamcode.CustomClasses.Position;
+
+public class AutoOne extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+    }
+
+}
